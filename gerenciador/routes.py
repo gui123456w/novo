0f49ed5e454f966fd90fd9e401738df515077041
GUILHERME SANTOS NASCIMENTO <guilherme.snascime10@senacsp.edu.br>
2026-06-16 17:20:06 -0300

    atualização do logout

--- gerenciador/routes.py
+++ gerenciador/routes.py
@@ -1,6 +1,6 @@
 from gerenciador import app, database, bcrypt
 from flask import render_template, url_for, redirect
-from flask_login import login_required, login_user, logout_user
+from flask_login import login_required, login_user, logout_user, current_user
 from gerenciador.forms import FormLogin, FormCriarConta
 
 from gerenciador.models import Usuario
@@ -8,7 +8,22 @@ from gerenciador.models import Usuario
 @app.route('/', methods=['GET', 'POST'])
 def index():
     formlogin=FormLogin()
+
+    if formlogin.validate_on_submit():
+        usuario = Usuario.query.filter_by(email=formlogin.email.data).first()
+
+        if usuario and bcrypt.check_password_hash(usuario.password, formlogin.password.data):
+            login_user(usuario, remember=True)
+            return redirect(url_for('gerenciador'))
+
+
     return render_template('index.html', form=formlogin)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/criarconta', methods=['GET', 'POST'])
 def criarconta():
